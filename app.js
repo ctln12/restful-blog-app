16c7cc417b499097351e9e8666975412c3db292b
Cataline Baert <snuf_kin@yahoo.com>
2020-09-17 10:55:43 +0200

    add configuration to avoid mongoose deprecation warning

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const express = require("express");
 const app = express();
 
 // APP CONFIG
-mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
