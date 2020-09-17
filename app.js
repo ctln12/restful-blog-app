758d4b8caa05dd48f814703f444976411a60c77b
Cataline Baert <snuf_kin@yahoo.com>
2020-09-17 10:14:44 +0200

    create destroy route, add delete and edit buttons to show page

--- app.js
+++ app.js
@@ -80,6 +80,16 @@ app.put("/blogs/:id", (req, res) => {
   });
 });
 
+app.delete("/blogs/:id", (req, res) =>{
+  Blog.findByIdAndRemove(req.params.id, (err) => {
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is running");
 });
